f5a7404242dd10b7400dab1db378d531f4b76a54
Frank <frank@steamedveggie.com>
2023-12-09 09:55:14 -0500

    More images and create get_random_image() function

--- view.php
+++ view.php
@@ -61,6 +61,14 @@ if (!$blocked) {
     $conn->commit();
 }
 
+function get_random_image() {
+    $rand = rand(0, 163);
+    if ($rand == 98) {
+        return get_random_image();
+    }
+    return $rand;
+}
+
 if ($blocked) {
     die("<!DOCTYPE html><html lang='en'><head><title>401 Unauthorized</title><link rel='stylesheet' type='text/css' href='resources/css/view.css'/><style>" .
             ".bg {\n" .
@@ -94,7 +102,7 @@ if ($blocked) {
         <style>
             .bg {
                 background-image: url("https://infotoast.org/images/<?php
-            echo rand(0, 135);
+            echo get_random_image();
 ?>.jpg");
                 background-size: 100% 100%;
                 width: 100%;
